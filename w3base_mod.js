ec30413522da13bdb465d35a71d76500234909ed
jlacerte <justinlacerte@yahoo.ca>
2018-01-03 11:22:23 -0500

    fist commit

---
+++ w3base_mod.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var dt = require('./firstmod');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
+}).listen(80); 
+
+console.log("server running")
